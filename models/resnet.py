aba2ba1283136a8163b057f7a2671d5acf31ec0f
Alexandre Senouci <alexandre.senouci-ext@bnf.fr>
2024-01-18 15:56:22 +0100

    continue hook

--- models/resnet.py
+++ models/resnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
 
+
 class BaseResNet18(nn.Module):
     def __init__(self):
         super(BaseResNet18, self).__init__()
@@ -11,38 +12,38 @@ class BaseResNet18(nn.Module):
     def forward(self, x):
         return self.resnet(x)
 
+
 def activation_shaping_hook(module, input, output):
-  z = torch.mul(input,ouput)
-  for i in range(z.size()[0]):
-    for j in range(z.size()[1]):
-      if z[i][j] != 0.0:
-        z[i][j] = 1.0
-  return z
+    m = torch.rand(output.size()[0], output.size()[1])
+    z = torch.mul(output, m)
+    for i in range(z.size()[0]):
+        for j in range(z.size()[1]):
+            if z[i][j] != 0.0:
+                z[i][j] = 1.0
+    return z
+
 
 class ASHResNet18(nn.Module):
     def __init__(self):
         super(ASHResNet18, self).__init__()
         self.resnet = resnet18(weights=ResNet18_Weights)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
-    
+
     def forward(self, x):
-
-      
         return self.resnet(x)
-
 
 ######################################################
 # TODO: either define the Activation Shaping Module as a nn.Module
-#class ActivationShapingModule(nn.Module):
-#...
+# class ActivationShapingModule(nn.Module):
+# ...
 #
 # OR as a function that shall be hooked via 'register_forward_hook'
-#def activation_shaping_hook(module, input, output):
-#...
+# def activation_shaping_hook(module, input, output):
+# ...
 #
 ######################################################
 # TODO: modify 'BaseResNet18' including the Activation Shaping Module
-#class ASHResNet18(nn.Module):
+# class ASHResNet18(nn.Module):
 #    def __init__(self):
 #        super(ASHResNet18, self).__init__()
 #        ...
