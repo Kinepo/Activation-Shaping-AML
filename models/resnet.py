fc3d49550218fe038b99a5596dab29207cb4a68f
Alexandre Senouci <alexandre.senouci-ext@bnf.fr>
2024-01-18 16:29:21 +0100

    test hook

--- models/resnet.py
+++ models/resnet.py
@@ -9,6 +9,10 @@ class BaseResNet18(nn.Module):
         self.resnet = resnet18(weights=ResNet18_Weights)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
 
+    def hook(model, input, output):
+        print("coucou")
+        return output
+
     def forward(self, x):
         return self.resnet(x)
 
