f64a7fcab8a40ab7e9ce257a6c8bfc15296d5562
Alexandre Senouci <alexandre.senouci-ext@bnf.fr>
2024-01-21 15:45:13 +0100

    pacs point 3

--- dataset/PACS.py
+++ dataset/PACS.py
@@ -1,7 +1,7 @@
 import torch
 import os
 import torchvision.transforms as T
-from dataset.utils import BaseDataset #, DomainAdaptationDataset, DomainGeneralizationDataset
+from dataset.utils import BaseDataset, DomainAdaptationDataset  # , DomainAdaptationDataset, DomainGeneralizationDataset
 from dataset.utils import SeededDataLoader
 
 from globals import CONFIG
@@ -48,7 +48,7 @@ def load_data():
             path, label = line[0].split('/')[1:], int(line[1])
             target_examples.append((os.path.join(CONFIG.dataset_args['root'], *path), label))
 
-        train_dataset = BaseDataset(source_examples, transform=train_transform)
+        train_dataset = DomainAdaptationDataset(source_examples, target_examples, transform=train_transform)
         test_dataset = BaseDataset(target_examples, transform=test_transform)
 
     ######################################################
