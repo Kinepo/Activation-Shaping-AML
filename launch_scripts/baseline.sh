3d8f6a3aa02aaf8a56f84aaf92653860cb87c140
Alexandre Senouci <alexandre.senouci-ext@bnf.fr>
2024-02-14 05:45:09 +0100

    sleep is useless

--- launch_scripts/baseline.sh
+++ launch_scripts/baseline.sh
@@ -1,6 +1,9 @@
 target_domain=${1}
 experiment_choose=${2}
 name_folder=${3}
+num_layer_choose=${4}
+num_block_choose=${5}
+num_bn_choose=${6}
 
 python main.py \
 --experiment=${experiment_choose} \
@@ -8,4 +11,7 @@ python main.py \
 --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
 --batch_size=128 \
 --num_workers=5 \
---grad_accum_steps=1
+--grad_accum_steps=1 \
+--num_layer = ${num_layer_choose} \
+--num_block = ${num_block_choose} \
+--num_bn = ${num_bn_choose}
--- models/resnet.py
+++ models/resnet.py
@@ -20,22 +20,48 @@ class ASHResNet18(nn.Module):
         self.resnet = resnet18(weights=ResNet18_Weights)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
 
-
     def point_2(self):
         def hook_2(module, input, output):
-                M = torch.randint(0, 2, output.size(), device=CONFIG.device)
-                Z = torch.mul(output, M)
-                Z = torch.where(Z > 0, 1.0, 0.0)
-                hook.remove()
-                return Z
+            M = torch.randint(0, 2, output.size(), device=CONFIG.device)
+            Z = torch.mul(output, M)
+            Z = torch.where(Z > 0, 1.0, 0.0)
+            hook.remove()
+            return Z
 
-        layer = self.resnet.layer4[0].bn1
-        hook = layer.register_forward_hook(hook_2)
+        match CONFIG.num_layer:
+            case 1:
+                layer = self.resnet.layer1[CONFIG.num_block]
+                if CONFIG.num_bn == 1:
+                    hook = layer.bn1.register_forward_hook(hook_2)
+                else:
+                    hook = layer.bn2.register_forward_hook(hook_2)
+            case 2:
+                layer = self.resnet.layer2[CONFIG.num_block]
+                if CONFIG.num_bn == 1:
+                    hook = layer.bn1.register_forward_hook(hook_2)
+                else:
+                    hook = layer.bn2.register_forward_hook(hook_2)
+            case 3:
+                layer = self.resnet.layer3[CONFIG.num_block]
+                if CONFIG.num_bn == 1:
+                    hook = layer.bn1.register_forward_hook(hook_2)
+                else:
+                    hook = layer.bn2.register_forward_hook(hook_2)
+            case 4:
+                layer = self.resnet.layer4[CONFIG.num_block]
+                if CONFIG.num_bn == 1:
+                    hook = layer.bn1.register_forward_hook(hook_2)
+                else:
+                    hook = layer.bn2.register_forward_hook(hook_2)
+            case _:
+                layer = self.resnet.layer4[0].bn1
+                hook = layer.register_forward_hook(hook_2)
+
         return None
 
     def forward(self, x):
-            self.point_2()
-            return self.resnet(x)
+        self.point_2()
+        return self.resnet(x)
 
 ######################################################
 # TODO: either define the Activation Shaping Module as a nn.Module
