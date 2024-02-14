39c47dbbeaac05db1122ee3044b4366cfa894169
Alexandre Senouci <alexandre.senouci-ext@bnf.fr>
2024-02-14 15:04:38 +0100

    script

--- launch_scripts/baseline.sh
+++ launch_scripts/baseline.sh
@@ -1,9 +1,17 @@
 target_domain=${1}
+experiment_choose=${2}
+name_folder=${3}
+num_layer_choose=${4}
+num_block_choose=${5}
+num_bn_choose=${6}
 
 python main.py \
---experiment=baseline \
---experiment_name=baseline/${target_domain}/ \
+--experiment=${experiment_choose} \
+--experiment_name=${experiment_choose}/${name_folder}/${target_domain}/ \
 --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
 --batch_size=128 \
 --num_workers=5 \
---grad_accum_steps=1
+--grad_accum_steps=1 \
+--num_layer=${num_layer_choose} \
+--num_block=${num_block_choose} \
+--num_bn=${num_bn_choose}
